9ab64b7aa48adb0164e816f13695e29c6a20ae01
witchcode <amirabasghasemi2022@gmail.com>
2024-07-17 14:01:17 -0700

    close sidebar left

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let sideLeft = document.querySelector('.side-left')
+
+function CloseSide() {
+    sideLeft.style.width = '0%'
+    sideLeft.style.opacity = '0'
+    // sideLeft.style.display = 'none'
+}
