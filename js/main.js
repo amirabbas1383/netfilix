22fa1b6ab4e4777f622368fbe7910317c05321fe
witchcode <amirabasghasemi2022@gmail.com>
2024-07-18 03:31:57 -0700

    sidebar open & close

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 let sideLeft = document.querySelector('.side-left')
+let main = document.querySelector('.main')
 
 function CloseSide() {
     sideLeft.style.width = '0%'
     sideLeft.style.opacity = '0'
+    main.style.marginLeft = '0%'
+    main.style.width = '95%'
     // sideLeft.style.display = 'none'
+}
+
+function OpenSide() {
+    sideLeft.style.width = '15%'
+    sideLeft.style.opacity = '1'
+    main.style.marginLeft = '15%'
+    main.style.width = '80%'
 }
